0924cb57547c8b6f8b77674837e78c096ad0ed0c
RasifMohammed <rasif@metrictreelabs.com>
2024-02-12 14:17:59 +0530

    db connection

--- app/settings.py
+++ app/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
 ]
 
 MIDDLEWARE = [
@@ -74,10 +75,15 @@ WSGI_APPLICATION = "app.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+  'default': {
+    # MySQL engine. Powered by the mysqlclient module.
+    'ENGINE': 'django.db.backends.mysql',
+    'NAME': 'setup',
+    'USER': 'rasif',
+    'PASSWORD': 'password',
+    'HOST': 'localhost',
+    'PORT': '3306',
+  }
 }
 
 
